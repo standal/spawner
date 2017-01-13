cf22584ea9e091fe0b46ee079985e2957f237915
Kardos Ferenc <kardos.ferenc5@gmail.com>
2017-01-13 11:31:48 +0100

    Update Spawner.lua

--- Spawner.lua
+++ Spawner.lua
@@ -13,3 +13,82 @@ local topline = paintutils.loadImage ("aLogo")
 local col = 2
 local row = 5
 local curMob = 0
+
+
+function listPage()
+	reloadMobs()
+	t= listMenu
+end
+
+function loadingPage()
+	t = loading
+end
+
+function moveMob(i, mobName)
+	loadingPage()
+	print("Moving mob:  ".. i .." "..mobName)
+	curMob = mobName
+	eggs.pushItem("north", i, 1, 1)
+	reloadMobs()
+end
+
+function spawnExact()
+	rs.setOutput("top", false)
+	sleep(1)
+	rednet.send(43, "switchState")
+	sleep(5)
+-- rednet.send(43, "switchState")
+	sleep(1)
+-- rs.setOutput("top", false)
+end
+
+function callButton(name)
+	if type(t.buttonList[name].func) == "function" then
+		t.buttonList[name].func()
+	end
+end
+
+function reloadMobs()
+	t= loading
+	local row = 5
+	local col = 2
+	sleep(1.5)
+	listMenu = touchpoint.new("left")
+	listMenu:add("Current Mob:  "..curMob, nil, 2, 2, 3, 8)
+	listMenu:add("Spawn Exact", spawnExact, 35,2, 50, 25)
+	for i=1,slotnum do
+		local tableInfo = eggs.getStackInSlot(i)
+
+		if tableInfo~=nil then
+			for key, value in pairs(tableInfo) do
+				if key == "captured" then
+					print(value .. " i = " .. i)
+					print(row)
+					listMenu:add(value, function() moveMob(i, value, function(
+					row = row + 2
+					end
+				end
+			else
+		end
+
+	end
+		t= listMenu
+end
+
+	loading:add("Loading...", nil,1,2,40,5,colors.gray, colors.gray)
+
+	listPage()
+
+	while true do
+		t:draw()
+			local event, p1 = t:handleEvents(os.pullEvent())
+			if event == "button_click" then
+			t:flash(p1)
+			t.buttonList[p1].func()
+			end
+		end
+
+
+
+
+
